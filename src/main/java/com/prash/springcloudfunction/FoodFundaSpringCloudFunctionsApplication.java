c69b46a86bef2c4113d25a44e07295b74da4f7ee
Prashant Jain <praz.jain@gmail.com>
2022-02-26 07:02:58 +0000

    Initial commit

---
+++ src/main/java/com/prash/springcloudfunction/FoodFundaSpringCloudFunctionsApplication.java
@@ -0,0 +1,13 @@
+package com.prash.springcloudfunction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodFundaSpringCloudFunctionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodFundaSpringCloudFunctionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prash/springcloudfunction/FoodFundaSpringCloudFunctionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prash.springcloudfunction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodFundaSpringCloudFunctionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
